338fe0c6245e85174b6fd31e6701f73da0c3205f
Ganikapudi, Shamsundar (Cyient) <shamsundar.ganikapudi@alcoa.com>
2026-08-19 10:26:20 +0530

    Added Group box

--- ExtractCautionsForEM/Form1.Designer.cs
+++ ExtractCautionsForEM/Form1.Designer.cs
@@ -31,15 +31,7 @@ namespace ExtractCautionsForEM
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnFetchCaution = new System.Windows.Forms.Button();
-            this.txtOutPutPath = new System.Windows.Forms.TextBox();
-            this.btnBrowseOutputPath = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.cmbModule = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtUrl = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -48,95 +40,26 @@ namespace ExtractCautionsForEM
             this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.lblHeader = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbModule = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowseOutputPath = new System.Windows.Forms.Button();
+            this.txtOutPutPath = new System.Windows.Forms.TextBox();
+            this.btnFetchCaution = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btnFetchCaution
-            // 
-            this.btnFetchCaution.BackColor = System.Drawing.SystemColors.Highlight;
-            this.btnFetchCaution.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
-            this.btnFetchCaution.ForeColor = System.Drawing.Color.White;
-            this.btnFetchCaution.Location = new System.Drawing.Point(332, 194);
-            this.btnFetchCaution.Name = "btnFetchCaution";
-            this.btnFetchCaution.Size = new System.Drawing.Size(185, 29);
-            this.btnFetchCaution.TabIndex = 1;
-            this.btnFetchCaution.Text = "Fetch Cautions";
-            this.btnFetchCaution.UseVisualStyleBackColor = false;
-            this.btnFetchCaution.Click += new System.EventHandler(this.btnFetchCaution_Click);
-            // 
-            // txtOutPutPath
-            // 
-            this.txtOutPutPath.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtOutPutPath.Location = new System.Drawing.Point(216, 158);
-            this.txtOutPutPath.Name = "txtOutPutPath";
-            this.txtOutPutPath.Size = new System.Drawing.Size(489, 22);
-            this.txtOutPutPath.TabIndex = 2;
-            // 
-            // btnBrowseOutputPath
-            // 
-            this.btnBrowseOutputPath.Location = new System.Drawing.Point(711, 158);
-            this.btnBrowseOutputPath.Name = "btnBrowseOutputPath";
-            this.btnBrowseOutputPath.Size = new System.Drawing.Size(68, 22);
-            this.btnBrowseOutputPath.TabIndex = 3;
-            this.btnBrowseOutputPath.Text = "Browse";
-            this.btnBrowseOutputPath.UseVisualStyleBackColor = true;
-            this.btnBrowseOutputPath.Click += new System.EventHandler(this.btnBrowseOutputPath_Click);
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(32, 229);
+            this.progressBar1.Location = new System.Drawing.Point(44, 255);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(747, 20);
             this.progressBar1.TabIndex = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(29, 91);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 14);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Select Module";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(29, 161);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(164, 14);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Choose Output Location";
-            // 
-            // cmbModule
-            // 
-            this.cmbModule.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbModule.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmbModule.FormattingEnabled = true;
-            this.cmbModule.Location = new System.Drawing.Point(216, 88);
-            this.cmbModule.Name = "cmbModule";
-            this.cmbModule.Size = new System.Drawing.Size(563, 22);
-            this.cmbModule.TabIndex = 7;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(29, 127);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(173, 14);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Enter Engine Manual Link";
-            // 
-            // txtUrl
-            // 
-            this.txtUrl.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUrl.Location = new System.Drawing.Point(216, 123);
-            this.txtUrl.Name = "txtUrl";
-            this.txtUrl.Size = new System.Drawing.Size(563, 22);
-            this.txtUrl.TabIndex = 9;
             // 
             // menuStrip1
             // 
@@ -210,23 +133,111 @@ namespace ExtractCautionsForEM
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtUrl);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cmbModule);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnBrowseOutputPath);
+            this.groupBox1.Controls.Add(this.txtOutPutPath);
+            this.groupBox1.Controls.Add(this.btnFetchCaution);
+            this.groupBox1.Location = new System.Drawing.Point(12, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(808, 177);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUrl.Location = new System.Drawing.Point(216, 62);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(563, 22);
+            this.txtUrl.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(29, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 14);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Enter Engine Manual Link";
+            // 
+            // cmbModule
+            // 
+            this.cmbModule.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbModule.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbModule.FormattingEnabled = true;
+            this.cmbModule.Location = new System.Drawing.Point(216, 27);
+            this.cmbModule.Name = "cmbModule";
+            this.cmbModule.Size = new System.Drawing.Size(563, 22);
+            this.cmbModule.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(29, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 14);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Choose Output Location";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(29, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 14);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Select Module";
+            // 
+            // btnBrowseOutputPath
+            // 
+            this.btnBrowseOutputPath.Location = new System.Drawing.Point(711, 97);
+            this.btnBrowseOutputPath.Name = "btnBrowseOutputPath";
+            this.btnBrowseOutputPath.Size = new System.Drawing.Size(68, 22);
+            this.btnBrowseOutputPath.TabIndex = 12;
+            this.btnBrowseOutputPath.Text = "Browse";
+            this.btnBrowseOutputPath.UseVisualStyleBackColor = true;
+            this.btnBrowseOutputPath.Click += new System.EventHandler(this.btnBrowseOutputPath_Click);
+            // 
+            // txtOutPutPath
+            // 
+            this.txtOutPutPath.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutPutPath.Location = new System.Drawing.Point(216, 97);
+            this.txtOutPutPath.Name = "txtOutPutPath";
+            this.txtOutPutPath.Size = new System.Drawing.Size(489, 22);
+            this.txtOutPutPath.TabIndex = 11;
+            // 
+            // btnFetchCaution
+            // 
+            this.btnFetchCaution.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnFetchCaution.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnFetchCaution.ForeColor = System.Drawing.Color.White;
+            this.btnFetchCaution.Location = new System.Drawing.Point(332, 133);
+            this.btnFetchCaution.Name = "btnFetchCaution";
+            this.btnFetchCaution.Size = new System.Drawing.Size(185, 29);
+            this.btnFetchCaution.TabIndex = 10;
+            this.btnFetchCaution.Text = "Fetch Cautions";
+            this.btnFetchCaution.UseVisualStyleBackColor = false;
+            this.btnFetchCaution.Click += new System.EventHandler(this.btnFetchCaution_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(832, 261);
+            this.ClientSize = new System.Drawing.Size(832, 299);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblHeader);
-            this.Controls.Add(this.txtUrl);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.cmbModule);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.btnBrowseOutputPath);
-            this.Controls.Add(this.txtOutPutPath);
-            this.Controls.Add(this.btnFetchCaution);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -235,21 +246,15 @@ namespace ExtractCautionsForEM
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private Button btnFetchCaution;
-        private TextBox txtOutPutPath;
-        private Button btnBrowseOutputPath;
         private ProgressBar progressBar1;
-        private Label label1;
-        private Label label2;
-        private ComboBox cmbModule;
-        private Label label3;
-        private TextBox txtUrl;
         private MenuStrip menuStrip1;
         private ToolStripMenuItem fileToolStripMenuItem;
         private ToolStripMenuItem exitToolStripMenuItem;
@@ -258,5 +263,14 @@ namespace ExtractCautionsForEM
         private Label lblHeader;
         private PictureBox pictureBox1;
         private ToolStripMenuItem helpToolStripMenuItem1;
+        private GroupBox groupBox1;
+        private TextBox txtUrl;
+        private Label label3;
+        private ComboBox cmbModule;
+        private Label label2;
+        private Label label1;
+        private Button btnBrowseOutputPath;
+        private TextBox txtOutPutPath;
+        private Button btnFetchCaution;
     }
 }
